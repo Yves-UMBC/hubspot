dcb25e2c5406f9d5fb560027a22549d85f6df8de
Yves-UMBC <101886444+Yves-UMBC@users.noreply.github.com>
2023-01-24 15:49:57 -0500

    Update datasetArchive.js

--- src/datasetArchive.js
+++ src/datasetArchive.js
@@ -1,14 +1,15 @@
 const api = require('./api')
 
-/* bent is a function that returns an async function.
-   bent takes options which constrain what is accepted by the client. Any response that falls outside the constraints will generate an error.
-   Checkout https://github.com/mikeal/bent for more
+/* 
+   - bent is a function that returns an async function.
+   - bent takes options which constrain what is accepted by the client. Any response that falls outside the constraints will generate an error.
+   - Checkout https://github.com/mikeal/bent for more
 */
 const bent = require('bent')
 
 let self = module.exports = {
     
-    // Separating the data access layer from the main service by making dataArchive a separate module
+    // Separating the data access layer from the main service (search) by making datasetArchive a separate module
     getData: async function(){
         const getJSON = bent('GET', 'json')
         return await getJSON(api.GET_ENDPOINT)
